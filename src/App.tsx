ca6914ea02088ce03966147f7d886b411f2da872
Rafael Jesus do Prado <rafaeljprado@gmail.com>
2021-12-09 17:12:29 -0300

    created modal

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,37 @@
 import React from 'react';
+import Modal from 'react-modal'
+import { useState } from "react";
 import { Dashboard } from './components/Dashboard';
 import { Header } from './components/Header';
 import { GlobalStyle } from './global';
 
 
+Modal.setAppElement('#root')
+
 export function App() {
+
+  const [isNewTransactionOpen, setIsNewTransactionOpen] = useState(false)
+
+  function handleOpenNewTransactionModal(){
+    setIsNewTransactionOpen(true)
+  }
+
+  function handleCloseNewTransactionModal(){
+    setIsNewTransactionOpen(false)
+  }
+
   return (
     <>
-      <Header />
+      <Header onOpenNewTransactionModal={handleOpenNewTransactionModal}/>
       <Dashboard />
+
+      <Modal 
+          isOpen={isNewTransactionOpen}
+          onRequestClose={handleCloseNewTransactionModal}
+        >
+          <h2>Cadastrar</h2>
+        </Modal>
+
       <GlobalStyle />
     </>
   );
